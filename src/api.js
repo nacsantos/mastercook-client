ce77ccad38a70f6f39eba5e286c79e19822185ba
nacsantos <nelsonacsantos@gmail.com>
2020-12-05 18:17:01 +0000

    Change apu url to https

--- src/api.js
+++ src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
 	//baseURL: "http://localhost:5000",
-	baseURL: "http://ipm-12-backend.herokuapp.com",
+	baseURL: "https://ipm-12-backend.herokuapp.com",
 });
