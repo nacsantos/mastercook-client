1f0caa586a3e66453bdfcd8fcd848d993d06e288
nacsantos <nelsonacsantos@gmail.com>
2020-12-08 12:36:46 +0000

    Fix colors

--- src/components/Checkout/CheckoutData1.js
+++ src/components/Checkout/CheckoutData1.js
@@ -5,107 +5,115 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Context } from "../../Context/CheckoutContext";
 
 import {
-	OrderDetails, OrderDetailsH6, ButtonMedium, H5
+	OrderDetails,
+	OrderDetailsH6,
+	ButtonMedium,
+	H5,
 } from "./CheckoutElements";
 
 import api from "../../api";
 import { withRouter, Redirect } from "react-router-dom";
 
 const total_order_ingredients_products = (order_ingredients_products) => {
-       
-    var order_ingredients_products_total = 0;
-    
-    var ingredient_product;
+	var order_ingredients_products_total = 0;
 
-    for (ingredient_product in order_ingredients_products) {
-        order_ingredients_products_total += parseFloat(order_ingredients_products[ingredient_product].price);
-    }
-    
-    return order_ingredients_products_total;
+	var ingredient_product;
 
+	for (ingredient_product in order_ingredients_products) {
+		order_ingredients_products_total += parseFloat(
+			order_ingredients_products[ingredient_product].price
+		);
+	}
+
+	return order_ingredients_products_total;
 };
 
 class Order extends Component {
 	constructor(props) {
-        super(props);
-        this.state = {
-            order_ingredients_products: [],
-            total: 0,
-            show: false,
-            redirect: false,
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
+		super(props);
+		this.state = {
+			order_ingredients_products: [],
+			total: 0,
+			show: false,
+			redirect: false,
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    componentDidMount(){
-        this.setState({
-            order_ingredients_products: JSON.parse(localStorage.getItem('checkoutList')),
-            total: Number(localStorage.getItem('checkoutTotal').substring(1, localStorage.getItem('checkoutTotal').length - 1))
-        })
-    }
+	componentDidMount() {
+		this.setState({
+			order_ingredients_products: JSON.parse(
+				localStorage.getItem("checkoutList")
+			),
+			total: Number(
+				localStorage
+					.getItem("checkoutTotal")
+					.substring(1, localStorage.getItem("checkoutTotal").length - 1)
+			),
+		});
+	}
 
-    componentDidUpdate(prevProps, prevState){
-        if(prevState.order_ingredients_products !== this.state.order_ingredients_products){
-            this.setState({
-                show: true
-            })
-        }
-    }
+	componentDidUpdate(prevProps, prevState) {
+		if (
+			prevState.order_ingredients_products !==
+			this.state.order_ingredients_products
+		) {
+			this.setState({
+				show: true,
+			});
+		}
+	}
 
-    handleClick() {
-        this.setState({
-            redirect: true
-        })
-    }
-    
-    render() {
+	handleClick() {
+		this.setState({
+			redirect: true,
+		});
+	}
 
-        if (this.state.redirect) {
-            return <Redirect to={{pathname: "/compareprice/x"}} />
-        }
-        
-        return (
-            <>
-                <h5 className="checkout-title">Order Details</h5>
-                <Container className="checkout-container2" fluid>
-                    <Row>
-                        <Col>
-                            <h6><b>Ingredient/Product</b></h6>
-                        </Col>
-                        <Col>
-                            <h6><b>Price</b></h6>
-                        </Col>
-                    </Row>
-                    {
-                        this.state.show ?
-                        this.state.order_ingredients_products.map(item => {
-                            console.log(item)
-                            return (
-                                <Row>
-                                    <Col>{item.ingredient}</Col>
-                                    <Col>{item.price}€</Col>
-                                </Row>
-                            );
-                        }) :
-                        <></>
-                    }
-                    <hr />
-                    <Row>
-                        <Col>
-                            <h5>Total: { 
-                                this.state.show ?
-                                this.state.total :
-                                ""
-                            } €</h5>
-                        </Col>
-                    </Row>
-                    <hr />
-                    <center>
-                            <ButtonMedium onClick={this.handleClick}>Edit Order</ButtonMedium>
-                    </center>
-                </Container>
-            </>
-        );
+	render() {
+		if (this.state.redirect) {
+			return <Redirect to={{ pathname: "/compareprice/x" }} />;
+		}
+
+		return (
+			<>
+				<h5 className="checkout-title">Order Details</h5>
+				<Container className="checkout-container2" fluid>
+					<Row>
+						<Col>
+							<OrderDetailsH6>Ingredient/Product</OrderDetailsH6>
+							{/* <h6><b>Ingredient/Product</b></h6> */}
+						</Col>
+						<Col>
+							<OrderDetailsH6>Price</OrderDetailsH6>
+						</Col>
+					</Row>
+					{this.state.show ? (
+						this.state.order_ingredients_products.map((item) => {
+							console.log(item);
+							return (
+								<Row>
+									<Col>{item.ingredient}</Col>
+									<Col>{item.price}€</Col>
+								</Row>
+							);
+						})
+					) : (
+						<></>
+					)}
+					<hr />
+					<Row>
+						<Col>
+							<h5>Total: {this.state.show ? this.state.total : ""} €</h5>
+						</Col>
+					</Row>
+					<hr />
+					<center>
+						<ButtonMedium onClick={this.handleClick}>Edit Order</ButtonMedium>
+					</center>
+				</Container>
+			</>
+		);
 	}
 }
 export default withRouter(Order);
--- src/components/Checkout/CheckoutElements.js
+++ src/components/Checkout/CheckoutElements.js
@@ -16,7 +16,7 @@ export const Icon = styled(Link)`
 	margin-left: 32px;
 	margin-top: 32px;
 	text-decoration: none;
-	color: #fff;
+	color: black;
 	font-weight: 700;
 	font-size: 32px;
 
@@ -37,8 +37,8 @@ export const FormContent = styled.div`
 `;
 
 export const OrderDetails = styled.div`
-	background: #AAAAAA;
-  color: #FFFFFF;
+	background: #aaaaaa;
+	color: black;
 	max-width: 400px;
 	height: auto;
 	width: 100%;
@@ -54,16 +54,17 @@ export const OrderDetails = styled.div`
 `;
 
 export const H5 = styled.h5`
-	color: #fff;
+	color: black;
 `;
 
 export const OrderDetailsH6 = styled.h6`
 	margin-bottom: 20px;
-	color: #fff;
+	color: black;
+	font-weight: bold;
 `;
 
 export const Form = styled.form`
-	background: #AAAAAA;
+	background: #aaaaaa;
 	height: auto;
 	width: 100%;
 	z-index: 1;
@@ -79,7 +80,7 @@ export const Form = styled.form`
 
 export const FormH1 = styled.h1`
 	margin-bottom: 20px;
-	color: #fff;
+	color: black;
 	font-size: 20px;
 	font-weight: 400;
 	text-align: center;
@@ -88,7 +89,7 @@ export const FormH1 = styled.h1`
 export const FormLabel = styled.label`
 	margin-bottom: 8px;
 	font-size: 14px;
-	color: #fff;
+	color: black;
 `;
 
 export const FormInputFull = styled.input`
@@ -96,7 +97,7 @@ export const FormInputFull = styled.input`
 	margin-bottom: 10px;
 	border: none;
 	border-radius: 4px;
-    width: 100%;
+	width: 100%;
 `;
 
 export const FormInputBig = styled.input`
@@ -104,7 +105,7 @@ export const FormInputBig = styled.input`
 	margin-bottom: 10px;
 	border: none;
 	border-radius: 4px;
-    width: 70%;
+	width: 70%;
 `;
 
 export const FormInputMedium = styled.input`
@@ -112,7 +113,7 @@ export const FormInputMedium = styled.input`
 	margin-bottom: 10px;
 	border: none;
 	border-radius: 4px;
-    width: 45%;
+	width: 45%;
 `;
 
 export const FormInputSmall = styled.input`
@@ -120,186 +121,184 @@ export const FormInputSmall = styled.input`
 	margin-bottom: 10px;
 	border: none;
 	border-radius: 4px;
-    width: 25%;
+	width: 25%;
 `;
 
 export const FormButtonBig = styled.button`
-	background: #E67F22;
+	background: #e67f22;
 	padding: 16px 0;
 	border: none;
 	border-radius: 4px;
-	color: #fff;
+	color: black;
 	font-size: 20px;
 	cursor: pointer;
-    width: 100%
+	width: 100%;
 `;
 
 export const ButtonMedium = styled.button`
-	background: #E67F22;
+	background: #e67f22;
 	padding: 16px 0;
 	border: none;
 	border-radius: 4px;
-	color: #fff;
+	color: black;
 	font-size: 20px;
 	cursor: pointer;
-    width: 75%
+	width: 75%;
 `;
 
-export const ContainerSingleRow = styled('div')`
-   width: 100%;
-   display: flex;
-   flex-flow: row wrap;
-   justify-content: space-between;
-   align-items: baseline;
- `;
+export const ContainerSingleRow = styled("div")`
+	width: 100%;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-between;
+	align-items: baseline;
+`;
 
 export const SelectStyles = {
-  container: base => ({
-    ...base,
-    flex: 1
-  })
+	container: (base) => ({
+		...base,
+		flex: 1,
+	}),
 };
 
 export const FormSelectState = styled.select`
-    padding: 10px 10px;
-    margin-bottom: 10px;
-    border: none;
-    border-radius: 4px;
-    padding-left: 5px;
-    font-size: 14px;
-    
-  option {
-    color: black;
-    background: white;
-    display: flex;
-    white-space: pre;
-    min-height: 20px;
-    padding: 0px 2px 1px;
-  }
+	padding: 10px 10px;
+	margin-bottom: 10px;
+	border: none;
+	border-radius: 4px;
+	padding-left: 5px;
+	font-size: 14px;
+
+	option {
+		color: black;
+		background: white;
+		display: flex;
+		white-space: pre;
+		min-height: 20px;
+		padding: 0px 2px 1px;
+	}
 `;
 
-export const CreditDebitCardSubform = styled.div`
-    
-`;
+export const CreditDebitCardSubform = styled.div``;
 
 export const FormSelectMonth = styled.select`
-    padding: 10px 10px;
-    margin-bottom: 10px;
-    border: none;
-    border-radius: 4px;
-    width: 60%;
-    padding-left: 5px;
-    font-size: 14px;
-    
-  option {
-    color: black;
-    background: white;
-    display: flex;
-    white-space: pre;
-    min-height: 20px;
-    padding: 0px 2px 1px;
-  }
+	padding: 10px 10px;
+	margin-bottom: 10px;
+	border: none;
+	border-radius: 4px;
+	width: 60%;
+	padding-left: 5px;
+	font-size: 14px;
+
+	option {
+		color: black;
+		background: white;
+		display: flex;
+		white-space: pre;
+		min-height: 20px;
+		padding: 0px 2px 1px;
+	}
 `;
 
 export const FormSelectYear = styled.select`
-    padding: 10px 10px;
-    margin-bottom: 10px;
-    border: none;
-    border-radius: 4px;
-    width: 35%;
-    padding-left: 5px;
-    font-size: 14px;
-    
-  option {
-    color: black;
-    background: white;
-    display: flex;
-    white-space: pre;
-    min-height: 20px;
-    padding: 0px 2px 1px;
-  }
+	padding: 10px 10px;
+	margin-bottom: 10px;
+	border: none;
+	border-radius: 4px;
+	width: 35%;
+	padding-left: 5px;
+	font-size: 14px;
+
+	option {
+		color: black;
+		background: white;
+		display: flex;
+		white-space: pre;
+		min-height: 20px;
+		padding: 0px 2px 1px;
+	}
 `;
 
 export const FormRadioWrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    align-items: baseline;
+	width: 100%;
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-between;
+	align-items: baseline;
 `;
 
 export const FormRadioWrapperCol = styled.div`
-    width: 100%;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    align-items: baseline;
+	width: 100%;
+	flex-flow: row wrap;
+	justify-content: space-between;
+	align-items: baseline;
 `;
 
 export const RadioMark = styled.span`
-  display: inline-block;
-  position: relative;
-  border: 1px solid #777777;
-  width: 14px;
-  height: 14px;
-  left: 0px;
-  border-radius: 50%;
-  margin-right: 5px;
-  vertical-align: middle;
-  &::after {
-    content: "";
-    display: block;
-    width: 0;
-    height: 0;
-    border-radius: 50%;
-    background-color: #E67F22;
-    opacity: 0;
-    left: 50%;
-    top: 50%;
-    position: absolute;
-    transition: all 110ms;
-  }
+	display: inline-block;
+	position: relative;
+	border: 1px solid #777777;
+	width: 14px;
+	height: 14px;
+	left: 0px;
+	border-radius: 50%;
+	margin-right: 5px;
+	vertical-align: middle;
+	&::after {
+		content: "";
+		display: block;
+		width: 0;
+		height: 0;
+		border-radius: 50%;
+		background-color: #e67f22;
+		opacity: 0;
+		left: 50%;
+		top: 50%;
+		position: absolute;
+		transition: all 110ms;
+	}
 `;
 
 export const RadioInput = styled.input`
-  position: absolute;
-  visibility: hidden;
-  display: none;
-  &:checked + ${RadioMark} {
-    &::after {
-      width: 10px;
-      height: 10px;
-      opacity: 1;
-      left: 10%;
-      top: 10%;
-    }
-  }
+	position: absolute;
+	visibility: hidden;
+	display: none;
+	&:checked + ${RadioMark} {
+		&::after {
+			width: 10px;
+			height: 10px;
+			opacity: 1;
+			left: 10%;
+			top: 10%;
+		}
+	}
 `;
 
 export const RadioLabel = styled.label`
-  display: flex;
-  cursor: pointer;
-  color: #FFFFFF;
-  font-size: 14px;
-  padding: 5px 10px 5px 0;
-  position: relative;
-  ${props =>
-    props.disabled &&
-    `
+	display: flex;
+	cursor: pointer;
+	color: #ffffff;
+	font-size: 14px;
+	padding: 5px 10px 5px 0;
+	position: relative;
+	${(props) =>
+		props.disabled &&
+		`
         cursor: not-allowed;
         opacity: 0.4;
     `}
 `;
 
 export const RadioText1 = styled.span`
-    margin-top: -5%;
-    display: flex;
+	margin-top: -5%;
+	display: flex;
 	color: #fff;
 	font-size: 14px;
 `;
 
 export const RadioText2 = styled.span`
-    margin-top: -2.5%;
-    display: flex;
+	margin-top: -2.5%;
+	display: flex;
 	color: #fff;
 	font-size: 14px;
 `;
